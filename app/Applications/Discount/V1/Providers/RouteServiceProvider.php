0815d6cb1d4ce4da1f5abdbce6ef81305309d9b9
Stefan <stefan.brankovik@gmail.com>
2018-09-19 00:19:16 +0200

    Install yarn and nodejs to parse the raml syntax for the endpoint docs

--- app/Applications/Discount/V1/Providers/RouteServiceProvider.php
+++ app/Applications/Discount/V1/Providers/RouteServiceProvider.php
@@ -6,6 +6,7 @@ namespace Discount\V1\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Laravel\Lumen\Application;
+use Laravel\Lumen\Routing\Router;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -18,6 +19,9 @@ class RouteServiceProvider extends ServiceProvider
     {
         $this->app->router->group(['prefix' => 'api/v1', 'as' => 'v1', 'middleware' => ['auth']], function ($router) {
             require __DIR__ . '/../api.php';
+        });
+        $this->app->router->get('api/v1/docs', function (Router $router) {
+            return view('docs');
         });
     }
 }
